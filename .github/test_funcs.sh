54f8aab914092698613808489dd621193c465c6d
Nelson Lojo <31297176+nelson-lojo@users.noreply.github.com>
2024-04-19 20:34:21 +0000

    it should just work?

--- .github/test_funcs.sh
+++ .github/test_funcs.sh
@@ -40,7 +40,7 @@ run_test() {
 run_all_tests() {
     START_LINE=$(($(grep -n "FUNCTION_CLASSES = {" src/function_classes/__init__.py  | cut -f1 -d:)+1))
     END_LINE=$(($(grep -n "}" src/function_classes/__init__.py  | cut -f1 -d:)-1))
-    LINES=$(sed -n "$START_LINE,$END_LINE p" src/function_classes/__init__.py)
+    LINES=$(sed -n "$(echo $START_LINE),$(echo $END_LINE) p" src/function_classes/__init__.py)
     FUNC_CLASSES=$(echo "$LINES" | cut -d\" -f2)
 
     echo -e "Detected the following function classes:\n$FUNC_CLASSES"
