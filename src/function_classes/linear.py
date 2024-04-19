ded86698ee1a6f47a8e0d74f261d98bd76eee0fc
Nelson Lojo <31297176+nelson-lojo@users.noreply.github.com>
2024-04-19 01:13:40 +0000

    revert unnecessary change

--- src/function_classes/linear.py
+++ src/function_classes/linear.py
@@ -19,6 +19,7 @@ class LinearRegression(FunctionClass):
 
     def evaluate(self, x_batch: torch.Tensor, *params: torch.Tensor) -> torch.Tensor:
         weights, *_ = params
+        # TODO: changed this line so it would run, but it's probably wrong!
         partial_sums = torch.bmm(weights.squeeze(-2), x_batch.permute(0, 2, 1))
         full_sums = torch.sum(partial_sums, dim=-2, keepdim=True)
         y_batch = full_sums.squeeze()
@@ -37,7 +38,7 @@ class SparseLinearRegression(LinearRegression):
         param_shape = self.p_dist.batch_shape + self.p_dist.event_shape
         mask = torch.ones(param_shape).bool()
         mask[torch.randperm(self.x_dim)[:self._sparsity]] = False
-        weights[mask] = 0
+        weights[:, mask] = 0
 
         return super().evaluate(x_batch, weights)
 
