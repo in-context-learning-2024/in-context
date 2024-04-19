de67c39545d39f1f15adef89454ec78ae6301772
Nelson Lojo <31297176+nelson-lojo@users.noreply.github.com>
2024-04-19 19:47:58 +0000

    iterating locally now, not ready

--- .github/test_funcs.sh
+++ .github/test_funcs.sh
@@ -5,25 +5,25 @@ prep_yaml() {
     KWARGS=""
 
     TEST_YAML="optim:
-        type: adam
-        lr: 0.0001
-    loss_fn:
-        type: squared
-    function_class: 
-        type: $FC_NAME$KWARGS
-    b_size: 8
-    seq_len: 20
-    x_dim: 10
-    x_dist:
-        type: normal
-    steps: 5
-    model:
-        type: gpt2
-        n_positions: 21
-        n_layer: 1
-        n_embd: 4
-        n_head: 1
-    "
+    type: adam
+    lr: 0.0001
+loss_fn:
+    type: squared
+function_class: 
+    type: $FC_NAME$KWARGS
+b_size: 8
+seq_len: 20
+x_dim: 10
+x_dist:
+    type: normal
+steps: 5
+model:
+    type: gpt2
+    n_positions: 21
+    n_layer: 1
+    n_embd: 4
+    n_head: 1
+"
 
     echo -e "$TEST_YAML" > conf/test.yml
 }
@@ -31,7 +31,7 @@ prep_yaml() {
 run_test() {
     TEST_NAME=$1
     conda run -n in-context-learning wandb offline
-    bash .github/prep_func_test_yaml.sh \""$TEST_NAME"\"
+    prep_yaml \""$TEST_NAME"\"
     conda run -n in-context-learning python src/ -c conf/test.yml
 }
 
