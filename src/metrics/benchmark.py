b19c779df25bec2e4bb90dc195cad02f8a517539
Nelson Lojo <31297176+nelson-lojo@users.noreply.github.com>
2024-04-21 06:39:16 +0000

    moved post processing outside of the function-class specific logic

--- src/metrics/benchmark.py
+++ src/metrics/benchmark.py
@@ -5,9 +5,9 @@ from typing import Iterable
 from core import ContextModel
 
 class Benchmark:
-	def __init__(self):
-		raise NotImplementedError("Abstract Class Benchmark cannot be instantiated!")
+    def __init__(self):
+        raise NotImplementedError("Abstract Class Benchmark cannot be instantiated!")
 
-	def evaluate(self, models: Iterable[ContextModel]) -> Iterable[Tensor]:
-		"""Produce a benchmark value for an iterable collection of ContextModels"""
-		raise NotImplementedError(f"Abstract Class Benchmark cannot evaluate!")
+    def evaluate(self, models: Iterable[ContextModel]) -> Iterable[Tensor]:
+        """Produce a benchmark value for an iterable collection of ContextModels"""
+        raise NotImplementedError(f"Abstract Class Benchmark cannot evaluate!")
