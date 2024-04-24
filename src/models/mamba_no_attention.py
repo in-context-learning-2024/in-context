a127e6d67d050ae87460bfad9f1f22f2a87cbb91
KesavaViswanadha <79319318+KesavaViswanadha@users.noreply.github.com>
2024-04-24 14:39:45 -0700

    Update mamba_no_attention.py

--- src/models/mamba_no_attention.py
+++ src/models/mamba_no_attention.py
@@ -8,122 +8,12 @@ import types
 from core import ContextModel
 
 from .attention_fns import vit_style_relu_attn, causal_relu_attn
-from .inferencing_fns import forward_GPT2Model
+from .inferencing_fns import forward_GPT2Model, block_var_declare_mamba_no_attention, forward_block_mamba_no_attention
 
 from transformers.modeling_outputs import BaseModelOutputWithPastAndCrossAttentions
 import functools
 
 
-
-# FOR REFERENCE: A GPT2 Block is initialized with the following code:
-
-# def __init__(self, config, layer_idx=None, attn_func=None):
-#         super().__init__()
-#         hidden_size = config.hidden_size
-#         inner_dim = config.n_inner if config.n_inner is not None else 4 * hidden_size
-
-#         self.ln_1 = nn.LayerNorm(hidden_size, eps=config.layer_norm_epsilon)
-#         self.attn = GPT2Attention(config, layer_idx=layer_idx, attn_func=attn_func)
-#         self.ln_2 = nn.LayerNorm(hidden_size, eps=config.layer_norm_epsilon)
-
-#         if config.add_cross_attention:
-#             self.crossattention = GPT2Attention(config, is_cross_attention=True, layer_idx=layer_idx, attn_func=attn_func)
-#             self.ln_cross_attn = nn.LayerNorm(hidden_size, eps=config.layer_norm_epsilon)
-
-#         self.mlp = GPT2MLP(inner_dim, config)
-# ----------------------------------------------------------------------
-# In the following function BLOCK VAR DECLARE, one can add new variables, remove unnecessary variables, and modify existing ones.
-# Then, modify the forward function as seen fit. 
-#
-# This is a way to modify layer architecture.
-
-#no cache
-def block_var_declare(self, this_mamba_model):
-    self.norm_f = this_mamba_model.norm_f
-    self.mamba_blocks = list(this_mamba_model.layers)
-    
-def forward_block(
-        self,
-        hidden_states: Optional[Tuple[torch.FloatTensor]],
-        layer_past: Optional[Tuple[torch.Tensor]] = None,
-        attention_mask: Optional[torch.FloatTensor] = None,
-        head_mask: Optional[torch.FloatTensor] = None,
-        encoder_hidden_states: Optional[torch.Tensor] = None,
-        encoder_attention_mask: Optional[torch.FloatTensor] = None,
-        use_cache: Optional[bool] = False,
-        output_attentions: Optional[bool] = False,
-        no_attention = False
-    ) -> Union[Tuple[torch.Tensor], Optional[Tuple[torch.Tensor, Tuple[torch.FloatTensor, ...]]]]:
-        
-        residual = hidden_states
-      
-        for mb in self.mamba_blocks:
-            hidden_states = mb(hidden_states)
-
-        hidden_states = self.norm_f(hidden_states)
-
-        hidden_states = residual + hidden_states
-
-        residual = hidden_states
-
-        hidden_states = self.ln_1(hidden_states)
-      
-        if not no_attention:
-            hidden_states = self.ln_1(hidden_states)
-    
-            attn_outputs = self.attn(
-                hidden_states,
-                layer_past=layer_past,
-                attention_mask=attention_mask,
-                head_mask=head_mask,
-                use_cache=use_cache,
-                output_attentions=output_attentions,
-            )
-            
-            attn_output = attn_outputs[0]  # output_attn: a, present, (attentions)
-            outputs = attn_outputs[1:]
-            # residual connection
-            hidden_states = attn_output + residual
-            
-            if encoder_hidden_states is not None and not no_attention:
-                # add one self-attention block for cross-attention
-                if not hasattr(self, "crossattention"):
-                    raise ValueError(
-                        f"If `encoder_hidden_states` are passed, {self} has to be instantiated with "
-                        "cross-attention layers by setting `config.add_cross_attention=True`"
-                    )
-                residual = hidden_states
-                hidden_states = self.ln_cross_attn(hidden_states)
-                cross_attn_outputs = self.crossattention(
-                    hidden_states,
-                    attention_mask=attention_mask,
-                    head_mask=head_mask,
-                    encoder_hidden_states=encoder_hidden_states,
-                    encoder_attention_mask=encoder_attention_mask,
-                    output_attentions=output_attentions,
-                )
-                attn_output = cross_attn_outputs[0]
-                # residual connection
-                hidden_states = residual + attn_output
-                outputs = outputs + cross_attn_outputs[2:] 
-                
-            residual = hidden_states
-        
-
-        feed_forward_hidden_states = self.mlp(hidden_states)
-
-        hidden_states = residual + feed_forward_hidden_states
-
-        if no_attention: 
-            outputs = (hidden_states,)
-        elif use_cache:
-            outputs = (hidden_states,) + outputs
-        else:
-            outputs = (hidden_states,) + outputs[1:]
-        
-        return outputs  # hidden_states, present, (attentions, cross_attentions)
-        
-      
 class MambaNoAttentionModel(ContextModel):
     def __init__(self, x_dim, n_positions, n_embd=128, n_layer=12, n_head=4, want_pos_embeddings=True, no_attention=False, custom_attn_func=None, num_mamba_layers=1, **kwargs):
         super(MambaNoAttentionModel, self).__init__()
@@ -168,8 +58,8 @@ class MambaNoAttentionModel(ContextModel):
 
         # Allow for changes in GPT2Block Forward Function
         for x in list(self._backbone.children())[3]:
-            x.forward = types.MethodType(functools.partial(forward_block, no_attention=no_attention), x)
-            block_var_declare(x, MambaModel(mamba_configuration))
+            x.forward = types.MethodType(functools.partial(forward_block_mamba_no_attention, no_attention=no_attention), x)
+            block_var_declare_mamba_no_attention(x, MambaModel(mamba_configuration))
 
         # Allow for changes in Attention function for GPT2Attention
         if self.custom_attn_func:
