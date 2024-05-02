b14045167dd126ca19745250cce224c43169bd72
KesavaViswanadha <79319318+KesavaViswanadha@users.noreply.github.com>
2024-05-02 13:51:56 -0700

    Update all_mod_archs.py

--- src/models/all_mod_archs.py
+++ src/models/all_mod_archs.py
@@ -9,7 +9,7 @@ from core import ContextModel
 
 from .mod_seq_model import ModSeqModel
 from .inferencing_fns import block_var_declare_mamba_single, forward_block_mamba_no_attention, forward_block_mambafirstformer, block_var_declare_mambaformer, forward_block_mambaformer, block_var_declare_no_change, forward_block_mod_transformer
-
+from .inferencing_fns_llama import block_var_declare_llamamamba, forward_block_llamamamba
 
 class MambaNoAttentionModel(ModSeqModel):
     def __init__(self, x_dim, n_positions, n_embd=128, n_layer=12, n_head=4, want_pos_embeddings=True, no_attention=False, custom_attn_func=None, num_mamba_layers=1, **kwargs):
@@ -68,3 +68,21 @@ class ModTransformerModel(ModSeqModel):
         self.name = f"mod_gpt2_embd={n_embd}_layer={n_layer}_head={n_head}"
       
         self.change_gpt2_block(block_var_declare_no_change, self.gpt2_configuration, forward_block_mod_transformer)
+
+class LlamaMambaModel(ModSeqModelLlama):
+    def __init__(self, x_dim, n_positions, n_embd=128, n_layer=12, n_head=4, want_rope=True, hidden_act="silu", rope_theta=1e4, num_mamba_layers=1, num_mamba_instances=2, **kwargs):
+        super(LlamaMambaModel, self).__init__(x_dim, n_positions, n_embd=n_embd, n_layer=n_layer, n_head=n_head, hidden_act=hidden_act, rope_theta=rope_theta)
+       
+        mamba_configuration = MambaConfig(
+            vocab_size=self.llama_configuration.vocab_size,
+            hidden_size=n_embd,
+            layer_norm_epsilon=self.llama_configuration.layer_norm_epsilon,
+            num_hidden_layers=num_mamba_layers,
+            use_cache=self.llama_configuration.use_cache
+        )
+        
+
+        self.name = f"llamamamba_embd={n_embd}_layer={n_layer}_head={n_head}"
+        
+        self.change_llama_block(block_var_declare_llamamamba, [MambaModel(mamba_configuration) for _ in range(num_mamba_instances)], forward_block_llamamamba)
+      
