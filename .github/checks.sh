1425c7d896db02150ed97d1cdb09f7e229be02c6
Nelson Lojo <31297176+nelson-lojo@users.noreply.github.com>
2024-05-02 17:36:13 -0700

    forgot to actually change CI run command

--- .github/checks.sh
+++ .github/checks.sh
@@ -48,7 +48,7 @@ run_check() {
 
     conda run -n in-context-learning wandb offline > /dev/null
     prep_yaml "$FUNC_NAME" "$MODEL_NAME"
-    WANDB_SILENT=true conda run -n in-context-learning python src/ -c conf/test.yml
+    WANDB_SILENT=true conda run -n in-context-learning python src/ -c conf/train/test.yml
     if [[ $? != 0 ]]; then return 1; fi
 
     echo -e "Passed check with model \"$MODEL_NAME\"" \
