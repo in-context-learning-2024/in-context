1b37d9c9188162f60b3d6f5601b770fe6ce61c6d
KesavaViswanadha <79319318+KesavaViswanadha@users.noreply.github.com>
2024-05-04 12:48:14 -0700

    Update all_mod_archs.py

--- src/models/all_mod_archs.py
+++ src/models/all_mod_archs.py
@@ -88,7 +88,7 @@ class LlamaMambaModel(ModSeqModelLlama):
         self.change_llama_block(block_var_declare_llamamamba, [MambaModel(mamba_configuration) for _ in range(num_mamba_instances)], forward_block_llamamamba)
 
 class ModLlamaModel(ModSeqModelLlama):
-    def __init__(self, x_dim, n_positions, n_embd=128, n_layer=12, n_head=4, want_rope=True, hidden_act="silu", custom_attn_func = None, rope_theta=1e4, num_mamba_layers=1, num_mamba_instances=2, **kwargs):
+    def __init__(self, x_dim, n_positions, n_embd=128, n_layer=12, n_head=4, want_rope=True, hidden_act="silu", custom_attn_func = None, rope_theta=1e4, **kwargs):
         super(LlamaMambaModel, self).__init__(x_dim, n_positions, n_embd=n_embd, n_layer=n_layer, custom_attn_func = custom_attn_func, n_head=n_head, hidden_act=hidden_act, rope_theta=rope_theta)
         
         self.name = f"llamamod_embd={n_embd}_layer={n_layer}_head={n_head}"
