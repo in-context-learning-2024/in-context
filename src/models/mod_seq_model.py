3fc4ac7f9143acf2158d57d1145d422a6535d8c0
KesavaViswanadha <79319318+KesavaViswanadha@users.noreply.github.com>
2024-05-04 12:41:43 -0700

    Update mod_seq_model.py

--- src/models/mod_seq_model.py
+++ src/models/mod_seq_model.py
@@ -52,13 +52,19 @@ class ModSeqModel(GPT2):
                 )
 
 class ModSeqModelLlama(Llama):
-    def __init__(self, x_dim, n_positions, n_embd=128, n_layer=12, n_head=4, want_rope=True, hidden_act="silu", rope_theta=1e4, **kwargs):
+    def __init__(self, x_dim, n_positions, n_embd=128, n_layer=12, n_head=4, want_rope=True, custom_attn_func=None, hidden_act="silu", rope_theta=1e4, **kwargs):
         super().__init__(x_dim, n_positions, n_embd=n_embd, n_layer=n_layer, n_head=n_head, hidden_act=hidden_act, rope_theta=rope_theta)
-        #if hidden_act == "silu":
-        #    self.custom_attn_func = functools.partial(forward_llama_attention_standard, want_rope=want_rope)
+        
+        if custom_attn_func = None:
+            self.attention_fn_template = forward_llama_attention_standard
+        
+        self.custom_attn_func = functools.partial(self.attention_fn_template, want_rope=want_rope)
 
         self.want_rope = want_rope
         self._n_dims = x_dim
+
+        for x in list((list(self._backbone.children())[1])):
+            x.self_attn.forward = types.MethodType(self.custom_attn_func, x.self_attn)
         
 
     def change_llama_block(self, instantiate_var_fn, instantiate_var_arg, instantiate_forward_fn):
@@ -68,4 +74,3 @@ class ModSeqModelLlama(Llama):
             x.forward = types.MethodType(functools.partial(instantiate_forward_fn), x)
             #EX: Block_var_declare_llamamamba
             instantiate_var_fn(x, instantiate_var_arg)
-            #x.self_attn.forward = types.MethodType(self.custom_attn_func, x.self_attn)
