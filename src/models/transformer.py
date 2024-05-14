248954aff8fb0b29cbe8750899cdfe59b56dc1bd
Nelson Lojo <nelson.lojo@berkeley.edu>
2024-05-14 02:01:08 +0000

    vocab size set to 1

--- src/models/transformer.py
+++ src/models/transformer.py
@@ -33,6 +33,7 @@ class GPT2(BackboneModel):
     def __init__(self, x_dim, n_positions, n_embd=128, n_layer=12, n_head=4, **kwargs):
 
         configuration = GPT2Config(
+            vocab_size=1,
             n_positions=2 * n_positions,
             n_embd=n_embd,
             n_layer=n_layer,
@@ -56,6 +57,7 @@ class Llama(BackboneModel):
     def __init__(self, x_dim, n_positions, n_embd=128, n_layer=12, n_head=4, hidden_act: str = 'silu', rope_theta: float = 1e4, **kwargs):
 
         configuration = LlamaConfig(
+            vocab_size=1,
             max_position_embeddings=2 * n_positions,
             hidden_size=n_embd,
             intermediate_size=4*n_embd,
@@ -63,7 +65,7 @@ class Llama(BackboneModel):
             num_attention_heads=n_head,
             hidden_act=hidden_act,
             rope_theta=rope_theta,
-            use_cache=True,
+            use_cache=False,
         )
 
         self.llama_configuration = configuration
