e05079746b936b8000d7f176171334d53e216a2b
DerrickhSun <49956063+DerrickhSun@users.noreply.github.com>
2024-05-18 00:30:15 +0000

    small modification to alg

--- src/metrics/benchmark.py
+++ src/metrics/benchmark.py
@@ -21,11 +21,14 @@ class Benchmark:
         raise NotImplementedError(f"Abstract Class Benchmark cannot evaluate!")
 
 class Regression_score(Benchmark):
+    # optimal is the optimal model for predicting function_class
+    # we can change this to call from a dictionary later
     def __init__(self, metric: Metric, function_class: FunctionClass, optimal: ContextModel):
         self.function_class = function_class
         self.metric = metric
         self.optimal = optimal
 
+    # returns reg_score for the given models
     def evaluate(self, models: Iterable[ContextModel]) -> Iterable[Tensor]:
         model_funct = FunctionClassError(self.metric, self.function_class)
         model_err = model_funct.evaluate(models)
@@ -40,10 +43,13 @@ class Regression_score(Benchmark):
 # errors for the zero estimator (seq_len,)
 # error for the optimal estimator (other batch size, seq_len)
 def reg_score(model_err, zero_err, optimal_err):
-    norm_model_err = torch.sub(model_err, zero_err)
-    norm_optimal_err = torch.sub(optimal_err, zero_err)
+    #norm_model_err = torch.sub(model_err, zero_err)
+    #norm_optimal_err = torch.sub(optimal_err, zero_err)
 
-    avg_model_err = torch.mean(norm_model_err, dim=(1,2))
-    avg_optimal_err = torch.mean(norm_optimal_err)
+    model_area = zero_err - model_err.mean(dim=1)
+    optimal_area = zero_err - optimal_err.mean(dim=0)
 
-    return avg_model_err - avg_optimal_err
+    #avg_model_err = torch.mean(norm_model_err, dim=(1,2))
+    #avg_optimal_err = torch.mean(norm_optimal_err)
+
+    return optimal_area/model_area
