00983c08150da1102fdbf57fe949544f2a66ecc9
Nelson Lojo <31297176+nelson-lojo@users.noreply.github.com>
2024-05-18 12:54:29 -0700

    switch some arguments to be kwargs, not part of argv

--- src/interactive/checkpoints.py
+++ src/interactive/checkpoints.py
@@ -3,5 +3,9 @@ from core import ContextModel
 from parse import parse_training_from_file
 
 def load_checkpoint_with_config(config_file: str, checkpoint_file: str) -> ContextModel:
-    trainer, _ = parse_training_from_file(config_file, "../conf/include", checkpoint_file)
+    trainer, _ = parse_training_from_file(
+        filename=config_file,
+        include="../conf/include",
+        checkpoint_path=checkpoint_file
+    )
     return trainer.model
