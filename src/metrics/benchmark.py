5ae217be2cd00410228595e03d2130eabd43f0fa
DerrickhSun <49956063+DerrickhSun@users.noreply.github.com>
2024-05-19 00:19:29 +0000

    revert benchmark.py

--- src/metrics/benchmark.py
+++ src/metrics/benchmark.py
@@ -2,10 +2,7 @@
 from torch import Tensor
 from typing import Iterable
 
-from core import (
-    FunctionClass,
-    ContextModel,
-)
+from core import ContextModel
 
 class Benchmark:
     def __init__(self):
@@ -14,5 +11,3 @@ class Benchmark:
     def evaluate(self, models: Iterable[ContextModel]) -> Iterable[Tensor]:
         """Produce a benchmark value for an iterable collection of ContextModels"""
         raise NotImplementedError(f"Abstract Class Benchmark cannot evaluate!")
-
-
