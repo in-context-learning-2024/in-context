3c24d48a7dfa42ecef3eda30bf886ccd5ef611fa
Nelson Lojo <31297176+nelson-lojo@users.noreply.github.com>
2024-05-20 01:53:27 +0000

    not quite right mask generation

--- src/function_classes/sparse_parity.py
+++ src/function_classes/sparse_parity.py
@@ -13,14 +13,20 @@ class SparseParityRegression(FunctionClass):
         super(SparseParityRegression, self).__init__(*args, **kwargs)
 
     def _init_param_dist(self) -> D.Distribution:
-        return RandomMaskDistribution(self.k, self.x_dim, self.batch_size)
+        return D.Categorical(
+            torch.ones(torch.Size([self.x_dim])) / self.x_dim
+        ).expand(torch.Size([self.batch_size, self.k]))
 
     def evaluate(self, x_batch: torch.Tensor, *params: torch.Tensor) -> torch.Tensor:
-        masks, *_ = params
+        selection_indices, *_ = params
 
-        # mask out xs with 1s
-        selected_x = x_batch * masks.unsqueeze(1) 
-        selected_x += 1 - masks.unsqueeze(1) 
+        # # mask out xs with 1s
+        # selected_x = x_batch * masks.unsqueeze(1) 
+        # selected_x += 1 - masks.unsqueeze(1)
+
+        selected_x = torch.zeros([self.batch_size, self.sequence_length, self.k])
+        for i in range(self.batch_size):
+            selected_x[i] = x_batch[i, :, selection_indices[i]]
 
         # multiply all xs to see the parity of non-masked out xs
         y_batch = torch.prod(selected_x, dim=2)
