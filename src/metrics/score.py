268b974dfe73ed38b7778f0d3ab76bc7c4785ea1
Nelson Lojo <nelson.lojo@berkeley.edu>
2024-05-21 04:58:43 +0000

    fixed regression score

--- src/metrics/score.py
+++ src/metrics/score.py
@@ -13,23 +13,24 @@ from .function_error import FunctionClassError
 
 class RegressionScore(Benchmark):
     # zero_err is of shape (1)
-    # optimal_err is of shape (seq_length,)
-    def __init__(self, metric: Metric, function_class: FunctionClass, zero_err, optimal_err):
+    # base_err is of shape (seq_length,)
+    def __init__(self, metric: Metric, function_class: FunctionClass, zero_err, base_err):
         self.funct_err = FunctionClassError(metric, function_class)
         self.zero_err = zero_err
-        self.optimal_err = optimal_err.mean(dim = 1)
+        self.base_err = base_err
 
-    def evaluate(self, models: Iterable[ContextModel]) -> Iterable[Tensor]:
+    def evaluate(self, models: Iterable[ContextModel], num_batches: int = 1) -> Iterable[Tensor]:
         # generating model errors
-        model_err = torch.Tensor(self.funct_err.evaluate(models))
-
-        # computations for reg_score
-        norm_model_err = torch.sub(model_err, self.zero_err)
-        norm_optimal_err = torch.sub(self.optimal_err, self.zero_err)
+        model_err = torch.tensor(self.funct_err.evaluate(models, num_batches=num_batches))
+        model_err = model_err.squeeze()
 
         # avg across batch_size
-        avg_model_err = torch.mean(norm_model_err, dim=1)
+        avg_model_err = torch.mean(model_err, dim=1)
 
+        norm_model_err = torch.sub(avg_model_err, self.zero_err)
+        norm_base_err = torch.sub(self.base_err, self.zero_err)
+        
         # divide across seq_length, then mean over seq_length to get score
-        scores = torch.div(avg_model_err, norm_optimal_err)
-        return torch.mean(scores, dim=1)
+        scores = torch.div(norm_model_err, norm_base_err)
+        scores = torch.mean(scores[:, 1:], dim=1) # we skip the first point because all models are trivially the zero estimator with no context
+        return scores
